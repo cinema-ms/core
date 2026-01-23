602119879c7eecc1f72bba7dbf78c6ed5a1bdb52
MaridiM <maridim92@gmail.com>
2026-01-23 03:06:55 +0300

    initial branch

---
+++ src/prettier.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('prettier').Config} */
+export default {
+    "arrowParens": "avoid",
+    "bracketSpacing": true,
+    "endOfLine": "auto",
+    "htmlWhitespaceSensitivity": "css",
+    "insertPragma": false,
+    "jsxBracketSameLine": false,
+    "jsxSingleQuote": true,
+    "printWidth": 120,
+    "proseWrap": "preserve",
+    "quoteProps": "as-needed",
+    "requirePragma": false,
+    "semi": false,
+    "singleQuote": true,
+    "tabWidth": 4,
+    "useTabs": true,
+    "trailingComma": "all",
+    "vueIndentScriptAndStyle": false,
+    "embeddedLanguageFormatting": "auto",
+    "importOrderSeparation": true,
+    "importOrderSortSpecifiers": true,
+    "importOrderCaseInsensitive": true,
+    "importOrderParserPlugins": [
+      "classProperties",
+      "decorators-legacy",
+      "typescript"
+    ],
+    "importOrder": [
+      "<THIRD_PARTY_MODULES>",
+      "^@(.*)$",
+      "^../(.*)$",
+      "^./(.*)$"
+    ],
+    "plugins": ["@trivago/prettier-plugin-sort-imports"]
+
+}
